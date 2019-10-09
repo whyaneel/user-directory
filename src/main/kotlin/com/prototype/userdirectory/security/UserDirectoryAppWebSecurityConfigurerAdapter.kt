a2e004943dbb1641a7414a29727fbc227b7ed899
whyaneel <whyaneel@gmail.com>
2019-10-10 00:11:30 +0800

    Firebase SDK Client Login and Firebase SDK Admin Verify for REST APIs
    move all static assets to assets folder.

---
+++ src/main/kotlin/com/prototype/userdirectory/security/UserDirectoryAppWebSecurityConfigurerAdapter.kt
@@ -0,0 +1,53 @@
+package com.prototype.userdirectory.security
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.builders.WebSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import com.google.firebase.FirebaseApp
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.firebase.FirebaseOptions
+import org.springframework.core.io.ClassPathResource
+import java.io.FileInputStream
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.auth.FirebaseAuth
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
+
+@EnableWebSecurity
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+class UserDirectoryAppWebSecurityConfigurerAdapter : WebSecurityConfigurerAdapter() {
+    override fun configure(web: WebSecurity) {
+        web.ignoring()
+            .antMatchers(HttpMethod.OPTIONS)
+            .antMatchers("/assets/**") // #3
+    }
+
+    override fun configure(http: HttpSecurity) {
+        http.csrf().disable()
+            .antMatcher("/api/**")
+            .addFilter(FirebaseAuthenticationFilter(firebaseAuth(), authenticationManager()))
+            .authorizeRequests()
+            .and()
+            .formLogin()  // #8
+            .loginPage("/index.html") // #9
+            .permitAll() // #5
+    }
+
+    @Bean
+    fun firebaseAuth(): FirebaseAuth {
+        val serviceAccount = ClassPathResource("userdirectory-service_account.json").inputStream
+        val defaultOptions = FirebaseOptions.builder().apply {
+            setCredentials(GoogleCredentials.fromStream(serviceAccount))
+        }.build()
+        val initializeApp = FirebaseApp.initializeApp(defaultOptions)
+        return FirebaseAuth.getInstance(initializeApp)
+    }
+}
